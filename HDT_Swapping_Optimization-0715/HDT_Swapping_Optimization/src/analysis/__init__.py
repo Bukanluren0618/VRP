ec90545008509aad91030fe6e4070a28e95d99d2
Bukanluren618 <wsywsy200@gmail.com>
2025-07-17 15:13:42 +1000

    版本更新

--- HDT_Swapping_Optimization-0715/HDT_Swapping_Optimization/src/analysis/__init__.py
+++ HDT_Swapping_Optimization-0715/HDT_Swapping_Optimization/src/analysis/__init__.py
@@ -1,17 +1,9 @@
-from . import post_analysis
-from .post_analysis import (
-    safe_value,
-    plot_road_network_with_routes,
-    plot_station_energy_schedule,
-    print_task_assignments,
-    print_vehicle_swap_nodes,
-    print_vehicle_routes,
-    plot_hdt_metrics,
-)
-from .pre_checks import check_task_feasibility
+from importlib import import_module
 
 __all__ = [
     "post_analysis",
+    "pre_checks",
+    "check_task_feasibility",
     "safe_value",
     "plot_road_network_with_routes",
     "plot_station_energy_schedule",
@@ -19,5 +11,14 @@ __all__ = [
     "print_vehicle_swap_nodes",
     "print_vehicle_routes",
     "plot_hdt_metrics",
-    "check_task_feasibility",
 ]
+
+_post_mod = None
+_pre_mod = None
+
+
+def _load_post():
+    """Import :mod:`post_analysis` on demand and cache the module."""
+    global _post_mod
+    if _post_mod is None:
+        _post_mod = import_module(".post_analysis", __name__)
